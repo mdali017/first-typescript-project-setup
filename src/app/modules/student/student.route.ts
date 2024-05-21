84152087befea203eb64f302383ee356d1350dc2
mdali017 <mohabbatalit8@gmail.com>
2024-05-21 19:22:15 +0600

    create sinlge student and get all student from ddatabase

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,3 +4,7 @@ import { StudentControllers } from './studnet.controller';
 const router = express.Router();
 
 router.post('/create-student', StudentControllers.createStudent);
+router.get('/', StudentControllers.getAllStudent);
+router.get('/:studentId', StudentControllers.getSingleStudent);
+
+export const StudentRoutes = router;
--- src/app/modules/student/studnet.controller.ts
+++ src/app/modules/student/studnet.controller.ts
@@ -3,8 +3,10 @@ import { StudentServices } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
-    const result = await StudentServices.createStudentIntoDB(student);
+    // const student = req.body;
+    // const { student } = req.body;
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
     res.status(200).json({
       success: true,
       message: 'Student is created succesfully',
@@ -15,6 +17,34 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Student are retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudent,
+  getSingleStudent,
 };
